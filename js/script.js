8989b84311702d3831e0f4619a85d9c9bf27e4a5
yukaemoto <yukaemtdeux@gmail.com>
2017-05-22 17:56:19 +0900

    画像など追加しました

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 $(function(){
-   
+   var count = $('.l-mainvisual li').length;
+    
+    var current = 1;
+    
+    var next = 2;
+    
+    var interval = 7000;
+    
+    var duration = 1000;
+    
+    var timer;
+    
+    $('.l-mainvisual li:not(:first-child)').hide();
+    
+    timer = setInterval(slideTimer,interval);
+    function slideTimer(){
+        $('.l-mainvisual li:nth-child(+' + current + ')').fadeOut(duration);
+        $('.l-mainvisual li:nth-child(+' + next + ')').fadeIn(duration);
+        
+        current = next;
+        next = ++next;
+        
+        if(next > count){
+            next = 1;
+        }
+    }
 })
